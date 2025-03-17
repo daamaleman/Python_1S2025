3e622daa8a3e6d059200d5b1870d9428edd5662b
daamaleman <diedereicha@uamv.edu.ni>
2025-03-17 10:36:25 -0600

    Sesion 1 17 de marzo

---
+++ exercise1.py
@@ -0,0 +1,10 @@
+#Comentario de una linea
+"""
+Imprimir los datos personales 
+del estudiante
+"""
+
+print("Datos del Estudiante")
+print("="*20)
+print("Nombre:", end=" ")
+print("José Alejadro")
